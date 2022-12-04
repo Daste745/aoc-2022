6ee12b0a56f5430bb75e2f30cb986af5ae03ac49
Daste <stefankar1000@gmail.com>
2022-12-04 09:01:39 +0100

    Add solution to day 04

---
+++ aoc/day04/part2.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+
+def range_from_str(range_str: str) -> range:
+    start, stop = map(int, range_str.split("-"))
+    return range(start, stop + 1)
+
+
+def main() -> None:
+    overlaps = 0
+
+    for line in stdin.readlines():
+        pairs = line.strip().split(",")
+        ranges = map(set, map(range_from_str, pairs))
+        range_1, range_2 = ranges
+
+        if (range_1 & range_2) != set():
+            overlaps += 1
+
+    print(overlaps)
+
+
+if __name__ == "__main__":
+    main()

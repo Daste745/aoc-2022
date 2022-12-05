147c813092641f149826445b43400e80e5b0c305
Daste <stefankar1000@gmail.com>
2022-12-05 10:23:03 +0100

    Add solution to day 05

---
+++ aoc/day05/part1.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+from sys import stdin
+
+
+def main() -> None:
+    stacks: defaultdict[int, list] = defaultdict(list)
+    lines = iter(stdin.readlines())
+
+    # Initial stacks
+    for line in lines:
+        if line == "\n":
+            break
+        # Assume they are sorted
+        if line.strip().startswith("1"):
+            continue
+
+        stack_data = (
+            line.replace("    ", "[.]")
+            .strip()
+            .replace(" ", "")
+            .replace("[", "")
+            .replace("]", "")
+        )
+        for i, container in enumerate(stack_data):
+            if container == ".":
+                continue
+            stacks[i].append(container)
+
+    for i in stacks.keys():
+        stacks[i].reverse()
+
+    # Moves
+    for line in lines:
+        clean_line = (
+            line.strip().replace("move ", "").replace(" from", "").replace(" to", "")
+        )
+        amount, from_stack, to_stack = map(int, clean_line.split(" "))
+        from_stack -= 1
+        to_stack -= 1
+
+        for i in range(amount):
+            stacks[to_stack].append(stacks[from_stack].pop())
+
+    output: list[str] = []
+    for i in sorted(stacks.keys()):
+        if len(stacks[i]) == 0:
+            output += " "
+        else:
+            output += stacks[i][-1]
+    print("".join(output))
+
+
+if __name__ == "__main__":
+    main()

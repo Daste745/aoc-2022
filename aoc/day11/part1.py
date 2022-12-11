3723e68c195ad1a0c58555f1adb1fb5fcf39cb57
Daste <stefankar1000@gmail.com>
2022-12-11 17:50:55 +0100

    Add solution to day 11

---
+++ aoc/day11/part1.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+from collections import Counter
+from dataclasses import dataclass
+from functools import reduce
+from itertools import islice
+from operator import mul
+from sys import stdin
+from typing import Iterator
+
+
+def chunk_by(iterator: Iterator, n: int) -> Iterator[list]:
+    while chunk := list(islice(iterator, n)):
+        yield chunk
+
+
+@dataclass
+class Monkey:
+    items: list
+    operator: str
+    operand: str
+    test: int
+    test_true: int
+    test_false: int
+    inspections: int = 0
+
+    def turn(self, monkeys: list[Monkey]) -> None:
+        for i in range(len(self.items)):
+            operand = self.items[i] if self.operand == "old" else int(self.operand)
+            if self.operator == "+":
+                self.items[i] += operand
+            elif self.operator == "*":
+                self.items[i] *= operand
+
+            self.items[i] //= 3
+
+            if self.items[i] % self.test == 0:
+                target = monkeys[self.test_true]
+            else:
+                target = monkeys[self.test_false]
+
+            target.items.append(self.items[i])
+            self.inspections += 1
+
+        self.items.clear()
+
+
+def main() -> None:
+    lines = iter(stdin.readlines())
+    monkeys: list[Monkey] = []
+
+    for data in chunk_by(lines, 7):
+        starting_items = map(int, data[1].split(":")[1].strip().split(", "))
+        operation = data[2].replace("Operation: new = old", "").strip()
+        operator, operand = operation.split(" ")
+        test_divisible_by = int(data[3].replace("Test: divisible by", "").strip())
+        test_true = int(data[4].replace("If true: throw to monkey", "").strip())
+        test_false = int(data[5].replace("If false: throw to monkey", "").strip())
+
+        monkeys.append(
+            Monkey(
+                items=list(starting_items),
+                operator=operator,
+                operand=operand,
+                test=test_divisible_by,
+                test_true=test_true,
+                test_false=test_false,
+            )
+        )
+
+    for _ in range(20):
+        for monkey in monkeys:
+            monkey.turn(monkeys)
+
+    top_inspections = Counter(
+        {i: monkey.inspections for i, monkey in enumerate(monkeys)}
+    )
+    print(top_inspections.most_common(2))
+    print(reduce(mul, map(lambda pair: pair[1], top_inspections.most_common(2))))
+
+
+if __name__ == "__main__":
+    main()
